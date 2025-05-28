db64873ebb9f8533bc2af94a355f6db958e1457d
Ajay Kumar Prasad <ajaykumar250604@gmail.com>
2025-05-29 01:15:25 +0530

    App.py added

--- app.py
+++ app.py
@@ -17,10 +17,11 @@ height = st.number_input("Height (cm)", min_value=50, max_value=250, value=170)
 weight = st.number_input("Weight (kg)", min_value=20, max_value=200, value=70)
 duration = st.number_input("Duration (minutes)", min_value=1, max_value=300, value=30)
 heart_rate = st.number_input("Heart Rate (bpm)", min_value=60, max_value=200, value=110)
-body_temp = st.number_input("Body Temperature (°F)", min_value=80.0, max_value=110.0, value=98.6)
+body_temp = st.number_input("Body Temperature (°C)", min_value=30.0, max_value=45.0, value=37.0)
+
 
 # Encode gender
-gender_encoded = 0 if gender == "Male" else 1
+gender_encoded = 1 if gender == "Male" else 0
 
 # Predict button
 if st.button("Predict"):
